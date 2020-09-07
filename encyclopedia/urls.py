fece1d6839e02872bac8012936d506a671df1765
Max Schaefer <prv.maxim@gmail.com>
2020-09-07 17:41:15 +0200

    Specicifation 'Random Page' finished

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<title>", views.entry, name="entry"),
-    path("new_page", views.new_page, name="new-page"),
+    path("random", views.random_entry, name="random-entry"),
+    path("new-entry", views.new_entry, name="new-entry"),
+    path("wiki/new-entry", views.new_entry),
     path("?q=<title>", views.entry)
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import markdown2
+import random
+from django import forms
 
 from . import util
+
+
+class NewTaskForm(forms.Form):
+    entryTitel = forms.CharField(label="Title")
+    entryContent = forms.CharField(widget=forms.Textarea, label="Content")
 
 
 def index(request):
@@ -12,10 +19,18 @@ def index(request):
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
-        "entry": markdown2.markdown(util.get_entry(title)),
-        "title": title
+        "entry": markdown2.markdown(util.get_entry(title))
     })
 
 
-def new_page(request):
-    return render(request, "encyclopedia/new_page.html")
+def new_entry(request):
+    return render(request, "encyclopedia/new-entry.html", {
+        "form": NewTaskForm
+    })
+
+
+def random_entry(request):
+    # Randomizer #
+    randomEntry = random.choice(util.list_entries())
+
+    return redirect(f'/wiki/{ randomEntry }')
