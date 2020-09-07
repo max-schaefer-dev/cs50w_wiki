01c0c413a4c020eb87f951c2bbf6e5867fd5ee3f
Max Schaefer <prv.maxim@gmail.com>
2020-09-08 00:54:20 +0200

    Specification 'New Page' finished

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 import markdown2
 import random
 import re
+
 from django import forms
 
 from . import util
 
 
-class NewTaskForm(forms.Form):
+class NewEntryForm(forms.Form):
     entryTitel = forms.CharField(label="Title")
     entryContent = forms.CharField(widget=forms.Textarea, label="Content")
 
@@ -50,19 +51,39 @@ def entry(request, title):
 
 def new_entry(request):
     if request.method == "POST":
-        search = request.POST.__getitem__("q")
-        results = []
-        for entry in util.list_entries():
-            if search.upper() == entry.upper():
-                return redirect(f'/wiki/{ search }')
-            mat = re.search(f'{ search.upper() }', entry.upper())
-            if mat:
-                results.append(entry)
-        return render(request, "encyclopedia/search-result.html", {
-            "results": results
-        })
+        if request.POST.__len__() == 3:
+            form = NewEntryForm(request.POST)
+            if form.is_valid():
+                entryTitle = request.POST.__getitem__("entryTitel")
+                for entry in util.list_entries():
+                    if entryTitle.upper() == entry.upper():
+                        return render(request, "encyclopedia/new-entry.html", {
+                            "error": "Error: This entry already exists!"
+                        })
+                entryContent = request.POST.__getitem__("entryContent")
+                md_file = f"entries\\{entryTitle}.md".format()
+                with open(md_file, 'w') as f:
+                    f.write(f"#{entryTitle}\n\n{entryContent}")
+                return redirect(f"/wiki/{ entryTitle }")
+
+        elif request.POST.__len__() == 2:
+            search = request.POST.__getitem__("q")
+            results = []
+            for entry in util.list_entries():
+                if search.upper() == entry.upper():
+                    return redirect(f'/wiki/{ search }')
+                mat = re.search(f'{ search.upper() }', entry.upper())
+                if mat:
+                    results.append(entry)
+            return render(request, "encyclopedia/search-result.html", {
+                "results": results
+            })
+        else:
+            return render(request, "encyclopedia/new-entry.html", {
+                "form": NewEntryForm
+            })
     return render(request, "encyclopedia/new-entry.html", {
-        "form": NewTaskForm
+        "form": NewEntryForm
     })
 
 
